38e1861da93c2ef1ed429f997725bf3b9b7ff08f
Nick Vermeer <nicholas.vermeer@gmail.com>
2013-01-24 17:43:34 -0500

    Cleanup

--- Keypoint_Tracker.cpp
+++ Keypoint_Tracker.cpp
@@ -23,7 +23,8 @@ int main(int ac, char ** av)
         help(av);
         return 1;
     }
-    OrbDescriptorExtractor descExtract;
+
+
     
     VideoCapture capture;
     capture.open(atoi(av[1]));
@@ -37,26 +38,26 @@ int main(int ac, char ** av)
         return 1;
     }
 
+    
     Mat rawframe,frame;
-
     vector<vector<DMatch> >matches_vect1;
     vector<vector<DMatch> >matches_vect2;
     vector<DMatch> matches;
+    OrbDescriptorExtractor descExtract;
     BFMatcher desc_matcher(NORM_HAMMING,false);
     
 
 
-    vector<Point2f> train_pts, query_pts,train_pts2,query_pts2;
     vector<KeyPoint> train_kpts, query_kpts;
     vector<unsigned char> match_mask;
-    Mat gray,gray2;
-    Mat image_out=Mat::zeros(1024,768,CV_8UC3);
+    Mat gray;
   
     Mat camera_matrix,distortion_coefficients,optimal_matrix;   
   
     bool ref_live = true;
     
     Mat train_desc, query_desc;
+
     const int DESIRED_FTRS = 800;
     GridAdaptedFeatureDetector detector(new FastFeatureDetector(),DESIRED_FTRS,3,3);
     FileStorage cam_storage("logi2_9000.yml",FileStorage::READ);
@@ -67,13 +68,14 @@ int main(int ac, char ** av)
     fn = cam_storage["distortion_coefficients"];
     distortion_coefficients = Mat((CvMat*)fn.readObj(),true);
     optimal_matrix=getOptimalNewCameraMatrix(camera_matrix,distortion_coefficients,Size(640,480),0);
+
+
     for (;;)
     {
         capture >> rawframe;
         if (rawframe.empty())
             break;
         undistort(rawframe,frame,camera_matrix,distortion_coefficients,optimal_matrix);
-        gray2=gray;
         cvtColor(frame, gray, CV_BGR2GRAY);
         
         detector.detect(gray, query_kpts); //Find interest points
@@ -87,14 +89,14 @@ int main(int ac, char ** av)
             desc_matcher.knnMatch(train_desc, query_desc, matches_vect2, 2);
             ratioTest(matches_vect2,0.65f);
             symmetryTest(matches_vect1,matches_vect2,matches);
-            matches2points(train_kpts, query_kpts, matches, train_pts, query_pts);
-            //drawKeypoints(frame, train_kpts, frame, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_OVER_OUTIMG);
+            drawKeypoints(frame, train_kpts, frame, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_OVER_OUTIMG);
             drawMatchesRelative(train_kpts, query_kpts, matches, frame, match_mask);
 
         }
 
         imshow("frame", frame);
-
+        imshow("gray", gray);
+        
         if (ref_live)
         {
             train_kpts = query_kpts;
